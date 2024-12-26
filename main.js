23e59014222a5bd3a61b7b585cf500f6fef178b1
JIKJACK <98028999+JIKJACK@users.noreply.github.com>
2024-12-26 12:20:12 +0700

    Update main.js

--- main.js
+++ main.js
@@ -2,43 +2,25 @@ const API_KEY = "API_GEMINI_KEY_HERE"; //เปลี่ยน "API_GEMINI_KEY_H
 const MODEL_NAME = "gemini-2.0-flash-thinking-exp";
 
 function doPost(e) {
-  // Check if the request contains required data
+  // Check if the request contains a prompt
   if (!e || !e.postData || !e.postData.contents) {
     return ContentService.createTextOutput(
-      JSON.stringify({ "error": "No data provided in the request." })
+      JSON.stringify({ "error": "No prompt provided in the request." })
     ).setMimeType(ContentService.MimeType.JSON);
   }
 
   const requestData = JSON.parse(e.postData.contents);
   const prompt = requestData.prompt;
   const systemInstruction = requestData.systemInstruction;
-  const history = requestData.history || ""; // Use empty string if no history
-
-    
-    const historyArray = history.split("\n").map(line => {
-    const parts = line.split(": ", 2); // Split into "role: text"
-
-        if (parts.length === 2){
-           const role = parts[0].trim();
-           const text = parts[1].trim();
-
-            if(role.toLowerCase() === "user" || role.toLowerCase() === "model") {
-                return {parts : [{text: text}] , role: role.toLowerCase() }
-            }
-        }
-        return null;
-    }).filter(item => item !== null);
-    
 
   const url = `https://generativelanguage.googleapis.com/v1alpha/models/${MODEL_NAME}:generateContent?key=${API_KEY}`;
 
-    const payload = {
-      contents: [
-          { parts: [{ text: systemInstruction }], role: "user" },
-          ...historyArray,
-          { parts: [{ text: prompt }], role: "user" }
-        ],
-    };
+  const payload = {
+    contents: [
+      { parts: [{ text: systemInstruction }], role: "user" },
+      { parts: [{ text: prompt }], role: "user" },
+    ],
+  };
 
   const options = {
     method: "POST",
@@ -83,6 +65,7 @@ function doPost(e) {
         JSON.stringify({ "error": `Error: ${response.getResponseCode()} - ${response.getContentText()}` })
       ).setMimeType(ContentService.MimeType.JSON);
     }
+
   } catch (e) {
     return ContentService.createTextOutput(
       JSON.stringify({ "error": `Error occurred: ${e}` })
